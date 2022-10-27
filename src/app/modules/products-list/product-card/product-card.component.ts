3df52df55f32ffb269199efe730758a86a578ae3
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-10-27 16:42:18 +0300

    feat: add product card

---
+++ src/app/modules/products-list/product-card/product-card.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { IProduct } from '../../../shared/products/product.interface';
+import { productMock } from '../../../shared/products/product.mock';
+
+@Component({
+	selector: 'app-product-card',
+	templateUrl: './product-card.component.html',
+	styleUrls: ['./product-card.component.less'],
+})
+export class ProductCardComponent {
+	readonly product = productMock;
+
+	onProductBuy(event: Event) {
+		event.stopPropagation();
+
+		console.log('Buy');
+	}
+
+	isStarActive(starIndex: number): boolean {
+		return Boolean(this.product && this.product.rating >= starIndex);
+	}
+}
--- src/app/modules/products-list/products-list.module.ts
+++ src/app/modules/products-list/products-list.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-	declarations: [ProductsListComponent],
-	imports: [CommonModule],
+	declarations: [ProductsListComponent, ProductCardComponent],
+	imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
 	exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
