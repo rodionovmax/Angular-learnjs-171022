b0e2011e76e7150de74cdcb77ebda6d79abd31cd
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-10-31 22:52:11 +0300

    feat: lecture 4

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+	AfterContentChecked,
+	AfterContentInit,
+	AfterViewChecked,
+	AfterViewInit,
+	Attribute,
+	Component,
+	DoCheck,
+	EventEmitter,
+	Input,
+	OnChanges,
+	OnDestroy,
+	OnInit,
+	Output,
+	SimpleChanges,
+} from '@angular/core';
 import { IApplicationConfig } from '../../shared/application-config/application-config.interface';
 
 @Component({
@@ -6,7 +21,55 @@ import { IApplicationConfig } from '../../shared/application-config/application-
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.less'],
 })
+// implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
 export class HeaderComponent {
 	@Input() applicationConfig: IApplicationConfig | undefined;
+	// @Input() config: IApplicationConfig | undefined;
 	@Output() menuClick = new EventEmitter<string | undefined>(true);
+
+	// constructor(
+	// @Attribute('size') private readonly sizeAttr: string,
+	// ) {
+	// console.log(this.sizeAttr);
+	// console.log('constructor', this.applicationConfig);
+	// }
+
+	// ngOnChanges({applicationConfig, config}: SimpleChanges) {
+	// 	if (applicationConfig) {
+	// 		// applicationConfig.currentValue === this.applicationConfig
+	// 		console.log('applicationConfig', applicationConfig);
+	// 	}
+
+	// 	if (config) {
+	// 		console.log('config', config)
+	// 	}
+	// }
+
+	// ngOnInit() {
+	// 	console.log('OnInit', this.applicationConfig);
+	// }
+
+	// ngDoCheck() {
+
+	// }
+
+	// ngAfterContentInit() {
+
+	// }
+
+	// ngAfterContentChecked() {
+
+	// }
+
+	// ngAfterViewInit() {
+
+	// }
+
+	// ngAfterViewChecked() {
+
+	// }
+
+	// ngOnDestroy() {
+
+	// }
 }
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,21 @@
-import { Component, ViewChild } from '@angular/core';
+import {
+	AfterContentInit,
+	AfterViewInit,
+	Component,
+	ContentChild,
+	ContentChildren,
+	ElementRef,
+	Input,
+	OnChanges,
+	OnInit,
+	QueryList,
+	SimpleChanges,
+	TemplateRef,
+	ViewChild,
+	ViewChildren,
+	ViewContainerRef,
+} from '@angular/core';
+import { MatList, MatListItem } from '@angular/material/list';
 import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
@@ -6,9 +23,67 @@ import { MatDrawer } from '@angular/material/sidenav';
 	templateUrl: './sidenav.component.html',
 	styleUrls: ['./sidenav.component.less'],
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit, AfterViewInit, AfterContentInit {
+	// @Input() set navigationTemplate(template: TemplateRef<unknown> | undefined) {
+	// 	// this.insertNavigationTemplate(template);
+	// 	this.viewport.clear();
+
+	// 	if (template) {
+	// 		this.viewport.createEmbeddedView(template);
+	// 	}
+	// };
+
 	@ViewChild(MatDrawer, { static: true })
 	private matDrawerInstance!: MatDrawer;
+	@ViewChild('viewport', { static: true, read: ViewContainerRef })
+	private viewport!: ViewContainerRef;
+
+	// @ContentChild('input', {static: true})
+	// private readonly input!: ElementRef<HTMLInputElement>;
+	@ContentChild('navigation', { static: true })
+	private readonly navigationContentTemplate!: TemplateRef<unknown>;
+
+	@ViewChildren('p')
+	private readonly p!: QueryList<HTMLElement>;
+
+	@ContentChildren(MatListItem, { read: ElementRef, descendants: false })
+	private readonly matListsElementRef!: QueryList<HTMLElement>;
+
+	// ngOnChanges({navigationTemplate}: SimpleChanges) {
+	// 	if (navigationTemplate) {
+	// 		this.insertNavigationTemplate(this.navigationTemplate)
+	// 	}
+	// }
+
+	// private insertNavigationTemplate(template: TemplateRef<unknown> | undefined) {
+	// 	this.viewport.clear();
+
+	// 	if (template) {
+	// 		this.viewport.createEmbeddedView(template);
+	// 	}
+	// }
+
+	ngOnInit() {
+		this.viewport.createEmbeddedView(this.navigationContentTemplate);
+		// console.log(this.input);
+		// this.input.nativeElement.value = '123';
+		// 	setTimeout(() => {
+		// 		if (this.navigationTemplate) {
+		// 			this.viewport.createEmbeddedView(this.navigationTemplate);
+		// 			this.viewport.createEmbeddedView(this.navigationTemplate);
+		// 			this.viewport.createEmbeddedView(this.navigationTemplate);
+		// 		}
+		// 	}, 4000);
+	}
+
+	ngAfterContentInit() {
+		console.log(this.matListsElementRef);
+		console.log(this.navigationContentTemplate);
+	}
+
+	ngAfterViewInit() {
+		console.log(this.p);
+	}
 
 	togleDrawer() {
 		this.matDrawerInstance.toggle();
